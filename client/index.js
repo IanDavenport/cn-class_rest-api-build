5f98fd242b8a27de2742bad9a631a9a543227f3c
Ian Davenport <davenporti632@gmail.com>
2020-10-13 16:28:27 +0100

    commit

---
+++ client/index.js
@@ -0,0 +1,27 @@
+
+
+const getBtn = document.getElementById('getData');
+const deleteBtn = document.getElementById('deleteData');
+
+
+getBtn.addEventListener('click', async() => {
+    let data = await fetch('http://localhost:3000/user/1');
+
+    console.log(await data.json());
+});
+
+
+
+deleteBtn.addEventListener('click', async() => {
+    let data = await fetch('http://localhost:3000/user/2', {
+        method: 'delete'
+    });
+    console.log(await data.json());
+});
+
+
+
+
+
+
+
